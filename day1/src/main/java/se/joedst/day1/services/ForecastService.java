44d0681994ff3813539f399c8486605bfa04705e
Joedst <joeledstrom@outlook.com>
2023-08-23 22:20:09 +0200

    Structural setup

---
+++ day1/src/main/java/se/joedst/day1/services/ForecastService.java
@@ -0,0 +1,10 @@
+package se.joedst.day1.services;
+
+import se.joedst.day1.models.Forecast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForecastService {
+    private static List<Forecast> forecasts = new ArrayList<Forecast>();
+}
